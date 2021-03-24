2824b6730dcdb8c2329f653c3ddeeb5655690b01
sookyung93 <i.sookyung93@gmail.com>
2021-03-24 16:53:54 +0900

    prevent mouse cursor of projects when stay other page

--- main.js
+++ main.js
@@ -11,17 +11,26 @@ navBarMenu.addEventListener('click', (event) => {
   activeButton(link);
 });
 
-// const categoryBtns = document.querySelector('.category__btn');
-// categoryBtns.addEventListener('click', (event) => {
-//   console.log('hi');
-//   console.log(event);
-// });
+const categoryBtns = document.querySelector('.work__categories');
+categoryBtns.addEventListener('click', (event) => {
+  console.log(event.target.dataset.filter);
+});
 
 function showSection(link) {
   const prevSection = document.querySelector('.show-section');
   prevSection.classList.remove('show-section');
   const newSection = document.querySelector(`${link}`);
   newSection.classList.add('show-section');
+
+  const projects = document.querySelector('.work__projects');
+  const categories = document.querySelector('.work__categories');
+  if (link === '#projects') {
+    projects.classList.add('show-projects');
+    categories.classList.add('show-btn');
+  } else {
+    projects.classList.remove('show-projects');
+    categories.classList.remove('show-btn');
+  }
 }
 
 function activeButton(link) {
@@ -31,9 +40,9 @@ function activeButton(link) {
   newSection.classList.add('active');
 }
 
-// function selectedCategory(category) {
-//   const prevBtn = document.querySelector('.selected');
-//   prevBtn.classList.remove('selected');
-//   const newBtn = document.querySelector(`[data-filter="${category}"]`);
-//   newBtn.classList.add('selected');
-// }
+function selectedCategory(category) {
+  const prevBtn = document.querySelector('.selected');
+  prevBtn.classList.remove('selected');
+  const newBtn = document.querySelector(`[data-filter="${category}"]`);
+  newBtn.classList.add('selected');
+}
